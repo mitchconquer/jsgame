ed06639b5e99c071bc948522200e0234fa7bbb53
mitchconquer <mitch@robotshanghai.com>
2016-07-13 08:43:50 -0700

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./js/game.js",
+  output: {
+    path: './',
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
