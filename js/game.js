4cf44785ada9eb11803a84a5c7e2a6e15faf0281
mitchconquer <mitch@robotshanghai.com>
2016-07-13 16:35:49 -0700

    Add internal grid to game class, create game loop

--- js/game.js
+++ js/game.js
@@ -3,38 +3,88 @@ const Canvas = require('./canvas'),
       Block = require('./block');
 
 
-function Game() {
+function Game(width = 300, height = 580) {
   this.gameOver = false;
+  this.width = width;
+  this.height = height;
+  this.roundsWon = 0;
+  this.roundsLost = 0;
+
+  /* Level data */
   this.level = 1;
   this.maxUpperGap = 280;
   this.minUpperGap = 35;
   this.ledgeRange = [15, 50];
 
-  while (!this.gameOver) {
-    this.playRound(level);
-  }
+  /* Round state */
+  this.roundIsSetup = false;
+  this.upperGap = undefined;
+  this.lowerGap = undefined;
+
+  this.gameLoop();
 }
 
-Game.prototype.playRound = function(levelNumber) {
-  const level = LevelConstants.get(levelNumber),
-        block = new Block(),
-        upperGap = this.randGap(),
-        upperWall = new UpperWall(upperGap),
-        lowerGap = upperGap - this.randLedge();
-        upperWall = new UpperWall(lowerGap);
+/* RENDERING */
 
-
-
-
+Game.prototype.height = function(newHeight) {
+  if (newHeight) {
+    this.height = newHeight;
+    return this.height;
+  }
+  return this.height;
 };
 
-Game.prototype.startGrowing = function (block) {
+Game.prototype.width = function(newWidth) {
+  if (newWidth) {
+    this.width = newWidth;
+    return this.width;
+  }
+  return this.width;
+};
+
+/* GAMEPLAY */
+
+Game.prototype.gameLoop = function() {
+  // Here can have logic for whether
+  // game continues, level advances, etc
+
+  if (!this.gameOver) {
+    this.setupRound();
+  }
+};
+
+Game.prototype.setupRound = function(levelNumber) {
+  const level = LevelConstants.get(levelNumber),
+        block = new Block((this.width / 2)),
+        this.upperGap = this.randGap(),
+        upperWall = new UpperWall(this.upperGap, this.width),
+        this.lowerGap = upperGap - this.randLedge();
+        upperWall = new UpperWall(this.lowerGap, this.width),
+        this.roundIsSetup = true;
+};
+
+Game.prototype.startGrowing = function(block) {
   block.startGrowing();
 };
 
 Game.prototype.stopGrowing = function(block) {
   block.stopGrowing();
+  this.roundIsSetup = false;
+  this.checkBlockSize(block);
 };
+
+Game.prototype.checkBlockSize = function(block) {
+  if (block.size > this.lowerGap && block.size < this.upperGap) {
+    this.roundsWon += 1;
+  } else {
+    this.roundsLost += 1;
+  }
+
+  // Restart the game loop
+  this.gameLoop();
+};
+
+/* GAME UTILS */
 
 Game.prototype.randGap = function() {
   return Math.random() * (this.maxUpperGap - this.minUpperGap) + this.minUpperGap;
@@ -43,6 +93,8 @@ Game.prototype.randGap = function() {
 Game.prototype.randLedge = function() {
   return Math.random() * (this.ledge[1] - this.ledge[0]) + this.ledge[0];
 };
+
+
 
 
 
